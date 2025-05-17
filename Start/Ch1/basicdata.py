345f0119761283e20952ff6e1bcf7f6109633feb
Phil S <47709040+MrPhil-S@users.noreply.github.com>
2025-05-17 00:08:31 +0000

    first commit

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -9,10 +9,36 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: What was the warmest day in the data set?
-
-
+#my attempt
+maxtemp = None
+for d in weatherdata:
+    if maxtemp:
+        temp = d['tmax']
+        if temp > maxtemp:
+            maxtemp = temp
+            day = d['date']
+    else:
+        maxtemp = d['tmax']
+        day = d['date']
+#print(f'I say the warmest day was {maxtemp} degrees on {day}')
+ 
+#using max function
+warmday = max(weatherdata, key=lambda x: x['tmax'])  #The lambda function is called over and over for each loop of weatherdata
+#print(f'The warmest was was {warmday['tmax']} on {warmday['date']}')
 # TODO: What was the coldest day in the data set?
 
+coldday = min(weatherdata, key=lambda x: x['tmin'])
+#print(f'The coolest was was {coldday['tmin']} on {coldday['date']}')
 
 # TODO: How many days had snowfall?
+count = 0
+for d in weatherdata:
+    if d['snow'] > 0.0:
+        count += 1
+#print(count)
 
+#do the same with list comprehension
+x = 0
+snow_days = [i for i in weatherdata if i['snow'] > 0.0]  #creates a list []
+print(f'Snow days: {len(snow_days)}')
+pprint.pp(snow_days)
--- Start/Ch1/filteringdata.py
+++ Start/Ch1/filteringdata.py
@@ -10,12 +10,37 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
 
 # the filter() function gives us a way to remove unwanted data points
 # TODO: create a subset of the data for days that had snowfall
+snows = [day for day in weatherdata if day['snow'] > 0]
+#pprint.pprint(snows)
 
 
-# TODO: pretty-print the resulting data set
+snowdays = list(filter(lambda  x: x['snow'] > 0, weatherdata))
+#pprint.pprint(snowdays)
+#print(f'Snow days: {len(snowdays)}')
 
 
 # filter can also be used on non-numerical data, like strings
-# TODO: create a subset that contains summer days with heavy rain (more than 1 in, about 2.5cm)
+# TODO: create a subset that contains summer days (july and Aug) with heavy rain (more than 1 in, about 2.5cm)
+# def is_summer_rain_day(d):
+#     summer_months = ['-07-', '-08-']
+#     if d[4:8] in summer_months:
+#         return True
+#     else:
+#         return False
+# summer_rain = list(filter(lambda x:x['prcp'] >= 1.0 and is_summer_rain_day(x['date']),weatherdata))
+# pprint.pp(summer_rain)
+# print(len(summer_rain))
+
 def is_summer_rain_day(d):
+    summer_months = ['-07-', '-08-']
+    if any([m in d['date'] for m in summer_months]) and d['prcp'] >= 1.0:
+        return True
     return False
+
+summer_rain = list(filter(is_summer_rain_day, weatherdata))
+
+pprint.pp(summer_rain)
+print(len(summer_rain))
+
+
+
